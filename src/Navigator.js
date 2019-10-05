811d19b8ae644cf1bb23f3c0378fdd1f47589545
VillePellosniemi <34025719+VillePellosniemi@users.noreply.github.com>
2019-10-05 15:05:07 +0300

    update

--- src/Navigator.js
+++ src/Navigator.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react'
 import Navbar from 'react-bootstrap/Navbar'
+import Button from 'react-bootstrap/Button';
+import FormControl from 'react-bootstrap/es/FormControl';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
 
 
 class Navigator extends Component {
   render() {
     return (
       <div>
-        <Navbar fixed="sticky-top" bg="primary" variant="dark">
-          <Navbar href="#home">Home</Navbar>
-            <Navbar href="#">valikko</Navbar>
-            <Navbar href="#">valikko</Navbar>
-            <Navbar href="#">valikko</Navbar>
-            <Navbar href="#">valikko</Navbar>
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="#home">
+            <img
+                alt=""
+                src="https://digitalillustrated.com/wp-content/uploads/2018/04/ikoni-_0030_rahka.png"
+                height={30}
+                className="d-inline-block align-top"
+            />
+            {' Rahkaryhmä'}
+          </Navbar.Brand>
+          <Nav className="mr-auto"> </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-info">Search</Button>
+          </Form>
         </Navbar>
       </div>
     )
--- src/Player.js
+++ src/Player.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import ReactPlayer from 'react-player'
 import './Player.css'
+import './input.css'
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Navigator from './Navigator';
@@ -13,6 +14,20 @@ class Player extends Component {
   state = {
     playing: true,
     controls: false,
+    playbackRate: 1.0,
+    played: 0,
+    volume: 0.8,
+    muted: false,
+    loaded: 0
+  }
+
+  load = url => {
+    this.setState({
+      url,
+      played: 0,
+      loaded: 0,
+      pip: false
+    })
   }
 
   handlePlayPause = () => {
@@ -27,9 +42,41 @@ class Player extends Component {
     screenfull.request(findDOMNode(this.player))
   }
 
+  handleSetPlaybackRate = e => {
+    this.setState({ playbackRate: parseFloat(e.target.value) })
+  }
+
+  handleSeekMouseDown = e => {
+    this.setState({ seeking: true })
+  }
+
+  handleSeekChange = e => {
+    this.setState({ played: parseFloat(e.target.value) })
+  }
+
+  handleSeekMouseUp = e => {
+    this.setState({ seeking: false })
+    this.player.seekTo(parseFloat(e.target.value))
+  }
+
+  handleVolumeChange = e => {
+    this.setState({ volume: parseFloat(e.target.value) })
+  }
+
+  handleToggleMuted = () => {
+    this.setState({ muted: !this.state.muted })
+  }
+
+  handleProgress = state => {
+    console.log('onProgress', state)
+    // We only want to update time slider if we are not currently seeking
+    if (!this.state.seeking) {
+      this.setState(state)
+    }
+  }
 
   render() {
-    const {controls, playing} = this.state
+    const {controls, playing, played, playbackRate, volume, muted, loaded} = this.state
 
     return (
         <Container>
@@ -46,20 +93,61 @@ class Player extends Component {
                              loop
                              controls={controls}
                              playing={playing}
-                             volume='0.75'
+                             playbackRate={playbackRate}
+                             volume={volume}
+                             muted={muted}
+                             onProgress={this.handleProgress}
                              width='100%'
                              height='100%'
-                             onReady={() => console.log('onReady')}
-                             onStart={() => console.log('onStart')}
-                             onPlay={this.handlePlay}
                 >
                 </ReactPlayer>
               </div>
+              <div className="divtest">
+                <p className="header">Played</p>
+                <progress className="progressbar" max={1} value={played} />
+              </div>
+              <div className="divtest">
+                <p className="header">Loaded</p>
+                <progress className="progressbar" max={1} value={loaded} />
+              </div>
             </Col>
             <Col sm={4} className="test">
-              <button onClick={this.handleStop}>Stop</button>
-              <button onClick={this.handlePlayPause}>{playing ? 'Pause' : 'Play'}</button>
-              <button onClick={this.handleClickFullscreen}>Fullscreen</button>
+              <div>
+                <p className="header">Controls</p>
+                <button className="button" onClick={this.handleStop}>Stop</button>
+                <button className="button" onClick={this.handlePlayPause}>{playing ? 'Pause' : 'Play'}</button>
+                <button className="button" onClick={this.handleClickFullscreen}>Fullscreen</button>
+              </div>
+
+              <br/>
+
+              <div>
+                <p className="header">Speed</p>
+                <button className="button" onClick={this.handleSetPlaybackRate} value={1}>1x</button>
+                <button className="button" onClick={this.handleSetPlaybackRate} value={1.5}>1.5x</button>
+                <button className="button" onClick={this.handleSetPlaybackRate} value={2}>2x</button>
+              </div>
+
+              <br/>
+
+              <div>
+                <p className="header">Seek</p>
+                <input
+                    type='range' min={0} max={1} step='any'
+                    value={played}
+                    onMouseDown={this.handleSeekMouseDown}
+                    onChange={this.handleSeekChange}
+                    onMouseUp={this.handleSeekMouseUp}
+                />
+              </div>
+
+              <br/>
+
+              <div>
+                <p className="header">Volume</p>
+                <input type='range' min={0} max={1} step='any' value={volume} onChange={this.handleVolumeChange} />
+              </div>
+
             </Col>
           </Row>
         </Container>
